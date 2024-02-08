8d94f9328e07028db0ba75cf15b96f5324aa135d
ipewzner <ipewzner@gmail.com>
2024-02-08 15:44:55 +0200

    7

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors= require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,10 +8,20 @@ const PORT = process.env.PORT || 3000;
 // Define the directory for static files (CSS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
+const allowHost=['https://thiala-df290042abc7.herokuapp.com/','https://tehilasargovski.com/']
+
+app.use(cors({
+  origin: allowHost,
+ 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //credentials:true,
+}));
+
 // Route to serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 app.post("/", (req, res) => {
